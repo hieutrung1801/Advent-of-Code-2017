a568bbd3dd18791e113558ce5c5dea4d4198cb49
Hieu Trung Huynh <hieutrung180196@gmail.com>
2017-12-05 20:55:39 +1000

    Day 2 solutions

--- Day 1/Day 1.py
+++ Day 1/Day 1.py
@@ -1,17 +1,20 @@
-""""You're standing in a room with "digitization quarantine" written in LEDs along one wall. The only door is locked, but it includes a small interface. "Restricted Area - Strictly No Digitized Users Allowed."
-
-It goes on to explain that you may only leave by solving a captcha to prove you're not a human. Apparently, you only get one millisecond to solve the captcha: too fast for a normal human, but it feels like hours to you.
-
-The captcha requires you to review a sequence of digits (your puzzle input) and find the sum of all digits that match the next digit in the list. The list is circular, so the digit after the last digit is the first digit in the list.
 """
+The captcha requires you to review a sequence of digits (your puzzle input) and find the sum of all digits that match the
+next digit in the list. The list is circular, so the digit after the last digit is the first digit in the list."""
 f = open("Puzzle.txt", "r")
+
 myNumber = f.readline()
 myArray = [int(d) for d in str(myNumber)]
-print(myArray)
-array_length = len(myArray)
-print(array_length)
+
 total1 = 0
 total2 = 0
+
+array_length = len(myArray)
+
+print(myArray)
+
+print(array_length)
+
 
 for index, number in enumerate(myArray):
     if index < array_length - 1:
@@ -22,10 +25,6 @@ for index, number in enumerate(myArray):
             total1 += myArray[0]
 
 """--- Part Two ---
-
-You notice a progress bar that jumps to 50% completion. Apparently, the door isn't yet satisfied, but it did emit a star as encouragement. 
-The instructions change:
-Now, instead of considering the next digit, it wants you to consider the digit halfway around the circular list.
 That is, if your list contains 10 items, only include a digit in your sum if the digit 10/2 = 5 steps forward matches it.
 Fortunately, your list has an even number of elements."""
 for index, number in enumerate(myArray):
--- Day 2/Day2- part 2.py
+++ Day 2/Day2- part 2.py
@@ -4,7 +4,6 @@ i = 0
 with open('Puzzle.txt') as f:
     array = [[int(x) for x in line.split()] for line in f]
 
-print(array[0])
 
 
 def quickSort(alist):
@@ -50,15 +49,26 @@ def partition(alist,first,last):
 
 
 length = len(array[0])
-while i < length:
-
-    print(length)
+stop = len(array)
+while i < stop:
     quickSort(array[i])
     print(array[i])
-    mysum += array[i][length - 1] - array[i][0]
-    print(mysum)
     i += 1
-    length = len(array[i])
 
+# for index, number in enumerate(array[0]):
+
+index = 0
+summed = 0
+while index < stop:
+    summed = 0
+    for x in array[index]:
+        for y in reversed(array[index]):
+            if (y % x == 0) and (x != y) and summed == 0:
+                summed = 1;
+                print('x and y ' + repr(x) + ' ' + repr(y))
+                mysum += y / x
+                print(mysum)
+    index += 1
+
+print('Even Sum')
 print(mysum)
-
